951238de8cbc074ffef5103e46a1fdb70a5c3b77
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2025-08-21 11:43:21 +1000

    Update Setup1.cmd

--- Setup1.cmd
+++ Setup1.cmd
@@ -1,5 +1,11 @@
 @echo on
+reg add "HKCU\Environment" /v "TEMP" /t REG_EXPAND_SZ /d "C:\TEMP" /f
+reg add "HKCU\Environment" /v "TMP" /t REG_EXPAND_SZ /d "C:\TEMP" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "1" /t REG_SZ /d "cmd.exe /c C:\TEMP\Setup2.cmd" /f
+powershell -noprofile -executionpolicy bypass -command "Set-NetFirewallProfile -Profile Private -LogFileName 'C:\TEMP\firewall_Private.log'"
+powershell -noprofile -executionpolicy bypass -command "Set-NetFirewallProfile -Profile Public -LogFileName 'C:\TEMP\firewall_Public.log'"
+powershell -noprofile -executionpolicy bypass -command "Set-WinHomeLocation -GeoId 0xb0"
+:: Wi-Fi
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" /v "Value" /t REG_SZ /d "Allow" /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" /v "ShowGlobalPrompts" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0000" /v "RegRoamLevel" /t REG_SZ /d "0" /f
@@ -18,5 +24,6 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0002" /v "PreferBand" /t REG_SZ /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0002" /v "PnPCapabilities" /t REG_DWORD /d "24" /f
 netsh wlan add profile filename="C:\TEMP\WLAN-LV426.xml" user=all
+:: finish
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 shutdown /r /t 5
