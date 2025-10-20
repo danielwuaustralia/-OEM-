750dbd77d5d6af3b257e6ea89509509cca9de88c
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2025-10-21 04:53:30 +1100

    Update Setup1.cmd

--- Setup1.cmd
+++ Setup1.cmd
@@ -3,13 +3,14 @@
 ::Desktop PC Special
 netsh wlan add profile filename="C:\TEMP\WLAN-LV426.xml" user=all
 powershell -noprofile -executionpolicy bypass -command "Set-NetAdapterAdvancedProperty -Name '*' -RegistryKeyword 'RegRoamLevel' -RegistryValue 1"
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "c:\pagefile.sys 32768 32768" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "c:\pagefile.sys 16384 16384" /f
 netsh wlan connect name=LV426
 :: finish
+powershell -noprofile -executionpolicy bypass -command "Set-WinHomeLocation -GeoId 0x4d"
 compact /compactos:never
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 pause
-start /b /w "C:\TEMP\UpdateTime.exe" /U /M
+"C:\TEMP\UpdateTime.exe" /U /M
 powershell -noprofile -executionpolicy bypass -command "certutil.exe -generateSSTFromWU C:\TEMP\roots.sst"
 powershell -noprofile -executionpolicy bypass -command "Get-ChildItem -Path C:\TEMP\roots.sst | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root"
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "1" /t REG_SZ /d "cmd.exe /c C:\TEMP\Setup2.cmd" /f
