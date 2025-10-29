12f399606b1a02f169232239c70e622c5d180f3f
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2025-10-30 00:45:26 +1100

    Update Setup1.cmd

--- Setup1.cmd
+++ Setup1.cmd
@@ -1,12 +1,18 @@
 @echo on
 
-::Desktop PC Special
+:: Desktop PC Special
 netsh wlan add profile filename="C:\TEMP\WLAN-LV426.xml" user=all
 powershell -noprofile -executionpolicy bypass -command "Set-NetAdapterAdvancedProperty -Name '*' -RegistryKeyword 'RegRoamLevel' -RegistryValue 1"
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "c:\pagefile.sys 32768 32768" /f
 netsh wlan connect name=LV426
 :: finish
-compact /compactos:never
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe update /verbose
+C:\Windows\microsoft.net\framework64\v4.0.30319\ngen.exe update /verbose
+compact /CompactOS:always
+compact /C /S:"C:\Program Files" /A /I /Q
+compact /C /S:"C:\Program Files (x86)" /A /I /Q
+compact /C /S:"C:\Windows" /A /I /Q
+compact /C /S:"C:\Users" /A /I /Q
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 pause
 "C:\TEMP\UpdateTime.exe" /U /M
