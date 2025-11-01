cd3674d2569ab7558074e15e8e195ad0f31d1362
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2025-11-01 16:19:03 +1100

    Modify Setup1.cmd for sound and OS settings
    Updated sound settings and compact OS behavior.

--- Setup1.cmd
+++ Setup1.cmd
@@ -9,17 +9,12 @@ netsh wlan connect name=LV426
 :: finish
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe update /verbose
 C:\Windows\microsoft.net\framework64\v4.0.30319\ngen.exe update /verbose
-compact /CompactOS:always
-compact /C /S:"C:\Program Files" /A /I /Q
-compact /C /S:"C:\Program Files (x86)" /A /I /Q
-compact /C /S:"C:\Windows" /A /I /Q
-compact /C /S:"C:\Users" /A /I /Q
+compact /CompactOS:never
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
-pause
 "C:\TEMP\UpdateTime.exe" /U /M
 powershell -noprofile -executionpolicy bypass -command "certutil.exe -generateSSTFromWU C:\TEMP\roots.sst"
 powershell -noprofile -executionpolicy bypass -command "Get-ChildItem -Path C:\TEMP\roots.sst | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root"
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "1" /t REG_SZ /d "cmd.exe /c C:\TEMP\Setup2.cmd" /f
 C:\TEMP\svcl.exe /SetVolume "扬声器" 100
-C:\TEMP\svcl.exe /SetDefaultFormat "扬声器" 24 48000
+C:\TEMP\svcl.exe /SetDefaultFormat "扬声器" 24 96000
 shutdown /r /t 5
